8617e8340583852ea2b75bd449f2407666d36933
Marcin Belzowski <marcin.belzowski@globallogic.com>
2018-10-22 17:20:31 +0200

    4

--- template/1.cpp
+++ template/1.cpp
@@ -2,46 +2,36 @@
 #include<vector>
 //using namespace std;
 
-
-
-template<class Char>
+template<typename Char>
 class String {
 public:
 String() {}                     //konstruktor
 
-
-String(const std::string &a)
+String(const std::string &a)    //konstruktor parametrowy
 {
-
 std:copy( a.begin(), a.end(), back_inserter(mojvector) );
-
 }
-
 
 
 String(String const &source)               //kopiujacy
 {
 mojvector=source.mojvector;
-
 }
-
 
 
 Char& operator[](int n)        //przeciazony operator []
 {
 return String::mojvector[n];
-} 
-
-
-String& operator+=(Char c)        //przeciazony operator +=
-{
-String::mojvector.push_back(c); 
 }
 
 
+void operator+=(Char c)        //przeciazony operator +=
+{
+String::mojvector.push_back(c);
+}
 
 
-bool operator==(const String &rhs)        //przeciazony operator +=
+bool operator==(const String &rhs)        //przeciazony operator ==
 {
 if(mojvector==rhs.mojvector)
     return true;
@@ -49,32 +39,28 @@ else
     return false;
 }
 
-
 private:
 std::vector<char> mojvector;           //Pole
-
-
 };
-
 
 
 int main()
 {
 
-String<char> a;
-a+='a';
+String<char> a;    //tworzymy obiekt a
+a+='a';            //przeci¹zonym operatorem += dodajemy elementy
 a+='b';
 a+='c';
 a+='d';
 
-std::cout<<a[2]<<std::endl;
+std::cout<<a[2]<<std::endl; // wyswietlamy przeciazonym operatorem [] 2 znak
 
-std::string dupa="hello";
+std::string dupa="hello";   // tworzymy string
 
-String<char> b(dupa);
+String<char> b(dupa);       // tworzymy nowy obiekt z wykorzystaniem
+                           // konstruktora kopiujacego
 
-
-if(a==dupa)
+if(a==dupa)                 // przeciazony operator ==
     std::cout<<"rowne"<<std::endl;
 else
     std::cout<<"rozne"<<std::endl;
