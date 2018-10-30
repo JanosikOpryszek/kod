467013f6eebd8198b5af49aa0b912bbafe2eeb90
Marcel <ahimm44@gmail.com>
2018-10-30 07:03:53 -0700

    domowe wilka

--- template/3.cpp
+++ template/3.cpp
@@ -2,13 +2,12 @@
 #include<vector>
 using namespace std;
 
+//zadanie matryca advanced 14
+
 template <int N,int M,typename T>
 struct Matrix
 {
-
-Matrix()
-{
-}    
+Matrix(){}    
 std::vector<vector<T> > matryca {N,vector<T>(M)};
 
 
@@ -17,32 +16,15 @@ void operator[] (int n)
     int rz=n/M;
     int nr=n%N;
     cout<<"rzad= "<<rz<<" kolumna= "<<nr<<endl;
-    cout<<matryca[rz][nr]<<endl;
+    cout<<matryca[rz-1][nr-1]<<endl;
 }
 
 /*
 void operator+=(int rzad,T)
 {
 }
-
-
-D& begin()
-{
-if(heap)
-    return mojav.front();
-else
-    return mojaa.front();
-}
-
-
-D& end()
-{
-if(heap)
-    return mojav.back();
-else
-    return mojaa.back();
-}
 */
+
 };
 
 
@@ -51,17 +33,18 @@ int main()
 {
 
 
-Matrix<5,5,int> aaa;
+Matrix<3,3,int> aaa; //{{1,2,3},{4,5,6},{7,8,9}};
 
-aaa[17];
+aaa.matryca[0].push_back(10);
+aaa.matryca[0].push_back(11);
+aaa.matryca[0].push_back(12);
 
-/*
-aaa+=10;
-aaa+=20;
-aaa+=30;
+aaa.matryca[1].push_back(13);
+aaa.matryca[1].push_back(14);
+aaa.matryca[1].push_back(15);
 
-std::cout<<aaa.begin()<<std::endl;
-*/
+aaa[5];
+
 
 return 0;
 }
