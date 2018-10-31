ee97aab87615cb6fa77a3e61ed7e144fd28dc17d
Marcel <ahimm44@gmail.com>
2018-10-31 09:55:44 +0100

    domowe3

--- template/3.cpp
+++ template/3.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 //zadanie matryca advanced 14
 
+
 template <int N,int M,typename T>
 struct Matrix
 {
 Matrix(){}    
 std::vector<vector<T> > matryca {N,vector<T>(M)};
+
+typename vector<vector<T> >::iterator it=matryca.begin();
+typename vector<T>::iterator it2=(*it).begin();
+
 
 
 void operator[] (int n)
@@ -16,14 +21,10 @@ void operator[] (int n)
     int rz=n/M;
     int nr=n%N;
     cout<<"rzad= "<<rz<<" kolumna= "<<nr<<endl;
-    cout<<matryca[rz-1][nr-1]<<endl;
+
+    cout<<*it2<<endl;
 }
 
-/*
-void operator+=(int rzad,T)
-{
-}
-*/
 
 };
 
@@ -33,17 +34,31 @@ int main()
 {
 
 
-Matrix<3,3,int> aaa; //{{1,2,3},{4,5,6},{7,8,9}};
+Matrix<3,3,int> aaa; 
 
-aaa.matryca[0].push_back(10);
-aaa.matryca[0].push_back(11);
-aaa.matryca[0].push_back(12);
+aaa.matryca[0][0]=10;
+aaa.matryca[0][1]=11;
+aaa.matryca[0][2]=12;
 
-aaa.matryca[1].push_back(13);
-aaa.matryca[1].push_back(14);
-aaa.matryca[1].push_back(15);
+aaa.matryca[1][0]=13;
+aaa.matryca[1][1]=14;
+aaa.matryca[1][2]=15;
 
-aaa[5];
+aaa.matryca[2][0]=16;
+aaa.matryca[2][1]=17;
+aaa.matryca[2][2]=18;
+
+//aaa[5];
+
+cout<<aaa.matryca.size()<<endl;
+cout<<aaa.matryca[0].size()<<endl;
+
+
+for( auto ele : aaa.matryca[0] )
+      cout<<ele<<endl;
+
+
+
 
 
 return 0;
