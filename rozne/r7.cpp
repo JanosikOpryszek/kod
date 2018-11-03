d1d593456bce55c8115706e4d300cc15cd57b954
JanosikOpryszek <ahimm44@gmail.com>
2018-11-03 11:48:55 -0700

    rozne wzajemne r6-r7

--- rozne/r7.cpp
+++ rozne/r7.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 //                         EXAMPLE   komponent - INTERFACE - dependency komponent 
 //                               wzajemne rejestrowanie obserwatora i obserwowanego podczas dodawania
+//                                                        MULTITHREAD
 
 
 class Interface1;                                       //Obserwowany Interface Forward declaration dla pointera
@@ -26,7 +27,7 @@ class Interface1 {                                       //Obserwowany Interface
 protected:
     std::list <Interface01*> obserwatorzy;
 public:
-    virtual void spiewaj()=0;                        //wirtualna metoda wywolywana przez obserwatora
+    virtual void spiewaj(int)=0;                        //wirtualna metoda wywolywana przez obserwatora
     void dodaj(Interface01 *o) {
         obserwatorzy.push_back (o);
         o->Ob=this;                                     //wpisuje adres obserwatora i podaje mu swoj adres do jego  zmiennej Ob
@@ -39,7 +40,7 @@ public:
 class DrStudent: public Interface01                       //Obserwator with interface , student's method pracuj is runing by interface
 {
 public:
- 
+    int licznik=0;
     int id=0;
     void pracuj ()
     {
@@ -47,17 +48,19 @@ public:
     while (licz--)
     {
     id+=1;
+    licznik++;
     cout<<id<<endl;                                    
- std::this_thread::sleep_for(std::chrono::milliseconds(100));
+std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    Ob->spiewaj();
+    Ob->spiewaj(licznik);
     };
 };
 
 
-class DrProfesor: public Interface01                       //Obserwator with second interface , Profesor's method pracuj is runing by interface
+class DrProfesor: public Interface01        //Obserwator with second interface , Profesor's method pracuj is runing by interface
 {
 public:
+    int licznik=0;
     int id=0;
     void pracuj ()
     {
@@ -65,10 +68,11 @@ public:
     while (licz--)
     {
     id-=1;
+    licznik--;
     cout<<id<<endl;                                    
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    Ob->spiewaj();
+    Ob->spiewaj(licznik);
     };
 };
 
@@ -77,23 +81,45 @@ public:
 
 class Komponent1:public Interface1                      //komponent obserwowany with interface 
 {
-    public:
-    void spiewaj()
-    {
-cout<<"TRALALALLALALALALALLA"<<endl;
-    }
-        
-    void inform()
-        {                            
-        std::list<Interface01 *>::iterator it;
-        std::vector<std::thread> myThreads;
+public:
+std::list<int> mojalista;       
 
-        for (it = obserwatorzy.begin(); it != obserwatorzy.end(); it++) 
-            {
-            (*it)->pracuj ();
-            //       myThreads.push_back(std::thread(&Interface01::pracuj, *it));     //nie dziala
-            }
+void spiewaj(int a)
+{
+mojalista.push_back(a);
+}
+
+void pokaz ()
+{
+while(1){
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+        for (auto element : mojalista)
+            cout<<element<<"'";
+        cout<<endl;
+        cout<<"lista:"<<endl;
         }
+}
+
+void inform()
+{                            
+std::list<Interface01 *>::iterator it;
+//        std::vector<std::thread> myThreads;
+
+for (it = obserwatorzy.begin(); it != obserwatorzy.end(); it++) 
+    {
+    (*it)->pracuj ();
+    //       myThreads.push_back(std::thread(&Interface01::pracuj, *it));     //nie dziala
+    }
+}
+
+void dzialaj()
+{
+while(1)
+    {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    this->inform();
+    }
+}
 };
 
 
@@ -111,19 +137,18 @@ Interface01* mojprofesor=new DrProfesor();         //create obserwator Profesor 
 mojkomponent->dodaj(mojstudent);                           //register next obserwator object in  obserwiowany list
 mojkomponent->dodaj(mojprofesor);                           //register next obserwator object in  obserwowany list
 
-//watki w c++
-//std::thread th(&Student::pracuj, mojstudent);
-//th.join();
 
+std::thread th(&Komponent1::dzialaj, mojkomponent);         //uruchom w watku mojkomponent.dzialaj();
+th.detach();                                                //ten watek pozwoli glownemu dzialac dalej
 
+std::thread th2(&Komponent1::pokaz, mojkomponent);          //uruchom w watku mojkomponent.pokaz();
+th2.join();                                                 //ten watek czeka na skonczenie (co nigdy nie nastapi)
 
-
-while(1)
+int ii=0;
+while(1)                                                    //tej petli nie wyswietli
 {
-cout<<"odliczanie:" <<endl;
-
- std::this_thread::sleep_for(std::chrono::milliseconds(100));
-   mojkomponent->inform();                                        //wysylaj powiadomienie do obserwatorow 
+cout<<"odliczanie: " <<ii++<<endl;
+std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 
