be053bfda0945f4a6dcd823e851c15bc049f6df9
Marcin Belzowski <marcin.belzowski@globallogic.com>
2018-11-21 14:50:02 +0100

    ETH2-2

---
+++ net/eth2/CMakeFiles/pr.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pr.dir/main.cpp.o"
+  "CMakeFiles/pr.dir/Ethernetdriverserver.cpp.o"
+  "CMakeFiles/pr.dir/MSGveryficator.cpp.o"
+  "pr.pdb"
+  "pr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
