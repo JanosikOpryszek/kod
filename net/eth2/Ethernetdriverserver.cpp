cf7659a02189a4e4a7d7e427644487b02a3e5f4b
Marcin Belzowski <marcin.belzowski@globallogic.com>
2018-11-22 13:36:09 +0100

    broadcast wer

--- net/eth2/Ethernetdriverserver.cpp
+++ net/eth2/Ethernetdriverserver.cpp
@@ -21,225 +21,168 @@ namespace drv
 {
 
 
-    pthread_mutex_t Ethernetdriverserver::mutexeth;     //mutex for pause & resume
-    pthread_t Ethernetdriverserver::thread_id;          //thread for main loop
-    eErrorCodes Ethernetdriverserver::retEr;          //variable to return errorcode
-    int32_t Ethernetdriverserver::server_sockfd;
-    int32_t Ethernetdriverserver::server_sockfd2;
-    int32_t Ethernetdriverserver::server_sockfd3;
-    int32_t Ethernetdriverserver::server_sockfd4;
-    char Ethernetdriverserver::bufferSS[ 4096 ];      //send
-    char Ethernetdriverserver::bufferRR[ 4096 ];      //recieve
-    sockaddr_in Ethernetdriverserver::server;
-    sockaddr_in Ethernetdriverserver::client1;
-    sockaddr_in Ethernetdriverserver::client2;
-    sockaddr_in Ethernetdriverserver::client3;
-    sockaddr_in Ethernetdriverserver::from;
-    socklen_t Ethernetdriverserver::len;
-    socklen_t Ethernetdriverserver::len2;
-    char Ethernetdriverserver::IpAdd1[14];
-    char Ethernetdriverserver::IpAdd2[14];
-    char Ethernetdriverserver::IpAdd3[14];
-    char Ethernetdriverserver::IpAdd4[14];
-    uint16_t Ethernetdriverserver::IpPort1;
-    uint16_t Ethernetdriverserver::IpPort2;
-    uint16_t Ethernetdriverserver::IpPort3;
-    uint16_t Ethernetdriverserver::IpPort4;
-    MSGveryficator *Ethernetdriverserver::msgverpointer;            // pointer to configurator
+    pthread_mutex_t Ethernetdriverserver::m_Mutexeth;     //mutex for pause & resume
+    pthread_t Ethernetdriverserver::m_Thread_id;          //thread for main loop
+    eErrorCodes Ethernetdriverserver::eRetEr;          //variable to return errorcode
+
+    int32_t Ethernetdriverserver::m_i32ServerSockfd;
+    int32_t Ethernetdriverserver::m_i32ServerSockfd2;
+
+    char Ethernetdriverserver::m_cBufferSS[ 4096 ];      //send
+    char Ethernetdriverserver::m_cBufferRR[ 4096 ];      //recieve
+    sockaddr_in Ethernetdriverserver::m_soServer;
+    sockaddr_in Ethernetdriverserver::m_soClient1;
+
+    sockaddr_in Ethernetdriverserver::m_soFrom;
+    socklen_t Ethernetdriverserver::m_Len;
+    socklen_t Ethernetdriverserver::m_Len2;
+    char Ethernetdriverserver::m_cIpAdd[14]="192.168.0.255";
+    uint16_t Ethernetdriverserver::m_u16IpPort=9742;
+
+    bool Ethernetdriverserver::m_bIsWorking=true;
+    MSGveryficator *Ethernetdriverserver::m_pMsgverpointer;            // pointer to configurator
     //add pointer to logger
     //add pointer to msgveryfikator
 
 
+Ethernetdriverserver::~Ethernetdriverserver()
+{
 
+
+}
 
 eErrorCodes Ethernetdriverserver::mStop()
-    {
-    retEr=OK;
-    //stop for shutdown code...
-    return retEr;
-    }
+{
+    eRetEr=OK;
+    m_bIsWorking=false;
+    return eRetEr;
+}     //Ethernetdriverserver::Shutdown()
 
 
 eErrorCodes Ethernetdriverserver::mResume()
-    {
-    retEr=OK;
-    pthread_mutex_unlock( &Ethernetdriverserver::mutexeth );
-    return retEr;
-    }    //Ethernetdriverserver::init()
+{
+    eRetEr=OK;
+    pthread_mutex_unlock( &Ethernetdriverserver::m_Mutexeth );
+    return eRetEr;
+}    //Ethernetdriverserver::init()
 
 
 eErrorCodes Ethernetdriverserver::mPause()
-    {
-    retEr=OK;
-    pthread_mutex_lock( &Ethernetdriverserver::mutexeth );
-    return retEr;
-    } //Ethernetdriverserver::deinit()
+{
+    eRetEr=OK;
+    pthread_mutex_lock( &Ethernetdriverserver::m_Mutexeth );
+    return eRetEr;
+} //Ethernetdriverserver::deinit()
 
 
 eErrorCodes Ethernetdriverserver::mRun()
-    {
-    retEr=OK;
+{
+    eRetEr=OK;
     //use configurator interface  and read config, 
-    eEcuNum EcuNr=ECU1; //run configurathot methos to set Ecu number
 
-    switch (EcuNr)
-    {
-    case ECU1:
-    {
-    strcpy(Ethernetdriverserver::IpAdd1,"192.168.0.103");
-    strcpy(Ethernetdriverserver::IpAdd2,"192.168.0.101");
-    strcpy(Ethernetdriverserver::IpAdd3,"192.168.0.113");
-    strcpy(Ethernetdriverserver::IpAdd4,"192.168.0.114");
-    Ethernetdriverserver::IpPort1=9742;
-    Ethernetdriverserver::IpPort2=9741;
-    Ethernetdriverserver::IpPort3=9743;
-    Ethernetdriverserver::IpPort4=9744;
-    break;
-    }
-    case ECU2:
-    {
-    strcpy(Ethernetdriverserver::IpAdd1,"192.168.0.112");
-    strcpy(Ethernetdriverserver::IpAdd2,"192.168.0.113");
-    strcpy(Ethernetdriverserver::IpAdd3,"192.168.0.114");
-    strcpy(Ethernetdriverserver::IpAdd4,"192.168.0.111");
-    Ethernetdriverserver::IpPort1=9742;
-    Ethernetdriverserver::IpPort2=9743;
-    Ethernetdriverserver::IpPort3=9744;
-    Ethernetdriverserver::IpPort4=9741;
-    break;
-    }
-    case ECU3:
-    {
-    strcpy(Ethernetdriverserver::IpAdd1,"192.168.0.113");
-    strcpy(Ethernetdriverserver::IpAdd2,"192.168.0.114");
-    strcpy(Ethernetdriverserver::IpAdd3,"192.168.0.111");
-    strcpy(Ethernetdriverserver::IpAdd4,"192.168.0.112");
-    Ethernetdriverserver::IpPort1=9743;
-    Ethernetdriverserver::IpPort2=9744;
-    Ethernetdriverserver::IpPort3=9741;
-    Ethernetdriverserver::IpPort4=9742;
-    break;
-    }
-    case ECU4:
-    {
-    strcpy(Ethernetdriverserver::IpAdd1,"192.168.0.114");
-    strcpy(Ethernetdriverserver::IpAdd2,"192.168.0.111");
-    strcpy(Ethernetdriverserver::IpAdd3,"192.168.0.112");
-    strcpy(Ethernetdriverserver::IpAdd4,"192.168.0.113");
-    Ethernetdriverserver::IpPort1=9744;
-    Ethernetdriverserver::IpPort2=9741;
-    Ethernetdriverserver::IpPort3=9742;
-    Ethernetdriverserver::IpPort4=9743;
-    break;
-    }
-
- }
     //start in thread inicialize main loop:
-    pthread_create(&Ethernetdriverserver::thread_id,0,&Ethernetdriverserver::initializess,this);
-    return retEr;
-    }
+    pthread_create(&Ethernetdriverserver::m_Thread_id,0,&Ethernetdriverserver::initializess,this);
+    return eRetEr;
+}
     
 
 eErrorCodes Ethernetdriverserver::setConfigurator()
-    {
-    retEr=OK;
+{
+    eRetEr=OK;
     //pass by methods argument pointer of pointer to configurator code...
-    return retEr;
-    }
+    return eRetEr;
+}
 
 
 eErrorCodes Ethernetdriverserver::setLogger()
-    {
-    retEr=OK;
+{
+    eRetEr=OK;
     //pass by methods argument pointer of pointer to logger code...
-    return retEr;
-    }
+    return eRetEr;
+}
 
 
-eErrorCodes Ethernetdriverserver::setMsgVeryficator(MSGveryficator *obj)
-    {
-    retEr=OK;
-    Ethernetdriverserver::msgverpointer=obj;
+eErrorCodes Ethernetdriverserver::setMsgVeryficator(MSGveryficator *a_Obj)
+{
+    eRetEr=OK;
+    Ethernetdriverserver::m_pMsgverpointer=a_Obj;
     //pass by methods argument pointer of pointer to msgveryficator code...
-    return retEr;
-    }
+    return eRetEr;
+}
 
 
 void *Ethernetdriverserver::initialize()    //void explen: - used static wrapper class to run pthreads
-    {
+{
     // (1) socket create for serwer
-    server_sockfd = socket(AF_INET,SOCK_DGRAM,0);       
+    m_i32ServerSockfd = socket(AF_INET,SOCK_DGRAM,0);
+    if (m_i32ServerSockfd <0 )
+    {
+        //logger - Socket create error                L O G I N G E R R O R
+
+    }
 
     //2 ACTIVATE STRUCTURE serwer me
-    server.sin_family      = AF_INET;
-    server.sin_addr.s_addr = inet_addr(IpAdd1);
-    server.sin_port        = htons(IpPort1); // port
+    m_soServer.sin_family      = AF_INET;
+    m_soServer.sin_addr.s_addr = static_cast<uint32_t>(INADDR_ANY);
+    m_soServer.sin_port        = htons(m_u16IpPort); // port
 
-    socklen_t len = sizeof( server );
-    //bind( server_sockfd, ( struct sockaddr * ) & server , len);   //old C style
-    bind( server_sockfd,reinterpret_cast<struct sockaddr*>(&server),len);
+    socklen_t len = sizeof( m_soServer );
+    if ((bind( m_i32ServerSockfd,reinterpret_cast<struct sockaddr*>(&m_soServer),len)) <0)
+    {
+        //logger Socket binding  error                L O G I N G E R R O R
+    }
 
-    while(1)                                                                   //main loop reading network
+    while(m_bIsWorking)                                                                   //main loop reading network
+    {
+        pthread_mutex_unlock( &Ethernetdriverserver::m_Mutexeth );             //mutex for pause & resume
+        memset( m_cBufferRR, 0, sizeof( m_cBufferRR ) );
+        if(recv( m_i32ServerSockfd, m_cBufferRR, sizeof( m_cBufferRR ), 0)<0)
         {
-        pthread_mutex_unlock( &Ethernetdriverserver::mutexeth );             //mutex for pause & resume
-        memset( bufferRR, 0, sizeof( bufferRR ) );
-        sockaddr_in from={};
-        //recvfrom( server_sockfd, bufferRR, sizeof( bufferRR ), 0,( struct sockaddr * ) & from, & len);      //old C style
-        recvfrom( server_sockfd, bufferRR, sizeof( bufferRR ), 0,reinterpret_cast<struct sockaddr *>(& from), & len);
+            //logger Socket recieving  error                L O G I N G E R R O R
+        }
 
         //std::cout<<bufferRR<<std::endl;
-        msgverpointer->mPutMessage(bufferRR);  // CALL MSGVERYFICATOR INTERFACE HERE 
+        m_pMsgverpointer->mPutMessage(m_cBufferRR);  // CALL MSGVERYFICATOR INTERFACE HERE
                                                //-  to pass MSG from network 
-        pthread_mutex_lock( &Ethernetdriverserver::mutexeth );
-        }
-    } //Ethernetdriverserver::intialize()
+        pthread_mutex_lock( &Ethernetdriverserver::m_Mutexeth );
+    }
+    return 0;
+} //Ethernetdriverserver::intialize()
 
 
 void *Ethernetdriverserver::initializess(void *context)
-    {
-    //return ( (Ethernetdriverserver *)context)->Ethernetdriverserver::initialize(); //old C style
+{
     return ( static_cast<Ethernetdriverserver *>(context))->Ethernetdriverserver::initialize();
-    } //Ethernetdriverserver::initializess
+} //Ethernetdriverserver::initializess
 
 
-eErrorCodes Ethernetdriverserver::send(std::string tab)                   //public interface method to let sending msg in network
-    {
-    retEr=OK;
+eErrorCodes Ethernetdriverserver::send(std::string a_strTab)                   //public interface method to let sending msg in network
+{
+    eRetEr=OK;
 
     // (1) socket create for client2
-    server_sockfd2 = socket(AF_INET,SOCK_DGRAM,0);       
-    // (1) socket create for client3
-    server_sockfd3 = socket(AF_INET,SOCK_DGRAM,0);       
-    // (1) socket create for client4
-    server_sockfd4 = socket(AF_INET,SOCK_DGRAM,0);       
- 
+    m_i32ServerSockfd2 = socket(AF_INET,SOCK_DGRAM,0);
+    if (m_i32ServerSockfd2<0)
+    {
+         //logger Socket 2 create error                L O G I N G E R R O R
+    }
+
     //2 ACTIVATE STRUCTURE client1
-    client1.sin_family      = AF_INET;
-    client1.sin_addr.s_addr = inet_addr(IpAdd2);
-    client1.sin_port        = htons(IpPort2); // port
-    //2 ACTIVATE STRUCTURE client2
-    client2.sin_family      = AF_INET;
-    client2.sin_addr.s_addr = inet_addr(IpAdd3);
-    client2.sin_port        = htons(IpPort3); // port
-    //2 ACTIVATE STRUCTURE client3
-    client3.sin_family      = AF_INET;
-    client3.sin_addr.s_addr = inet_addr(IpAdd4);
-    client3.sin_port        = htons(IpPort4); // port
+    m_soClient1.sin_family      = AF_INET;
+    m_soClient1.sin_addr.s_addr = inet_addr(m_cIpAdd);
+    m_soClient1.sin_port        = htons(m_u16IpPort); // port
 
-    socklen_t len2  = sizeof(client1 );
-    strcpy (bufferSS,tab.c_str()  );
+    socklen_t len2  = sizeof(m_soClient1 );
+    strcpy (m_cBufferSS,a_strTab.c_str()  );
 
-//    sendto( server_sockfd2, bufferSS, strlen( bufferSS ), 0,( struct sockaddr * ) & client1, len2);  //old C style
-//    sendto( server_sockfd3, bufferSS, strlen( bufferSS ), 0,( struct sockaddr * ) & client2, len2);
-//    sendto( server_sockfd4, bufferSS, strlen( bufferSS ), 0,( struct sockaddr * ) & client3, len2);
-    sendto( server_sockfd2, bufferSS, strlen( bufferSS ), 0,reinterpret_cast< struct sockaddr * >(& client1), len2);
-    sendto( server_sockfd3, bufferSS, strlen( bufferSS ), 0,reinterpret_cast< struct sockaddr * >(& client2), len2);
-    sendto( server_sockfd4, bufferSS, strlen( bufferSS ), 0,reinterpret_cast< struct sockaddr * >(& client3), len2);
+    if((sendto( m_i32ServerSockfd2, m_cBufferSS, strlen( m_cBufferSS ), 0,reinterpret_cast< struct sockaddr * >(& m_soClient1), len2))<0)
+    {
+        //logger Socket 2 create error                L O G I N G E R R O R
+    }
 
-
-    memset( bufferSS, 0, sizeof( bufferSS ) );
-    return retEr;
-    } //Ethernetdriverserver::send
+    memset( m_cBufferSS, 0, sizeof( m_cBufferSS ) );
+    return eRetEr;
+} //Ethernetdriverserver::send
 
 
 } //namespace drv
