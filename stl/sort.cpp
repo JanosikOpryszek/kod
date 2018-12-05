c82447c6e67a6ed8aa64b7cf415c51ffb22f4bc2
Marcel <ahimm44@gmail.com>
2018-12-05 11:03:51 +0100

    sort

---
+++ stl/sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+vector<string> mojva={"alaaaaaaaa","Alaaa","ALaaaaa","ala","ALAaa"};
+
+void sortuj(std::vector<string> &v )
+{
+std::sort(v.begin(),end(v), [](string ss,string ss2) {    return ss.length()<ss2.length();         }  ); 
+}
+
+int main ()
+{
+sortuj(mojva);
+
+for (auto element : mojva)
+    cout<<element<<endl;;
+
+return 0;
+}
+
+
+
+
