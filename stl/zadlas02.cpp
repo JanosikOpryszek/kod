6f50e78348b0e08a7c966a96cec401fa7edd390a
Lab User <user@wi.zut.pl>
2018-12-23 17:32:14 +0100

    zadlast

---
+++ stl/zadlas02.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<algorithm>
+
+
+using namespace std;
+
+
+void licz( )
+{
+int licz;
+vector<float> mojva;
+
+cout<<"podaj liczbe"<<endl;
+    cin>>licz;
+float licz2;
+cout<<"podawaj kolejne liczby float"<<endl;
+
+std::cin.exceptions(std::ios::failbit | std::ios::badbit);
+
+for(int i=0;i<licz;i++)
+{
+    cin>>licz2;
+    mojva.push_back(licz2); 
+}
+
+if (std::is_sorted( mojva.begin(),mojva.end() ) )
+{
+  cout<<  std::accumulate  (mojva.begin(),end(mojva),1.0f,std::multiplies<float>() ) << endl;
+
+
+}
+
+}
+
+
+int main ()
+{
+
+ licz();
+return 0;
+}
+
+
+
+
---
+++ stl/zadlas03.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstdio>
+
+
+using namespace std;
+
+
+void copiuj( )
+{
+
+
+vector<uint8_t> mojv={10,20,30,40};
+
+
+    
+cout<<std::hex<<;
+
+for (auto element :mojv )
+    cout<<(int)element<<endl;;
+
+}
+
+
+int main ()
+{
+
+
+copiuj();
+
+return 0;
+}
+
+
+
+
