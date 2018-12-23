ce55121ed87bd29d7bcf538861c53540bc9036da
JanosikOpryszek <ahimm44@gmail.com>
2018-12-23 09:50:58 -0800

    zadlas03-2

--- stl/zadlas03.cpp
+++ stl/zadlas03.cpp
@@ -8,11 +8,22 @@ using namespace std;
 void copiuj( )
 {
 vector<uint8_t> mojv={10,20,30,40};
-    
+ 
+std::ios::fmtflags old = std::cout.flags();
+
 cout<<std::hex<<endl;
 
 for (auto element :mojv )
     cout<<(int)element<<endl;;
+
+std::cout.setf(old);
+cout<<"powrot do OLD"<<endl;
+
+
+for (auto element :mojv )
+    cout<<(int)element<<endl;;
+
+
 }
 
 int main ()
