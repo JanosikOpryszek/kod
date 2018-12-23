aab46a732c05af8f16ab2688764b375dd8b8abc4
JanosikOpryszek <ahimm44@gmail.com>
2018-12-23 09:29:09 -0800

    zadlas03

--- stl/zadlas03.cpp
+++ stl/zadlas03.cpp
@@ -3,32 +3,21 @@
 #include<algorithm>
 #include<cstdio>
 
-
 using namespace std;
-
 
 void copiuj( )
 {
-
-
 vector<uint8_t> mojv={10,20,30,40};
-
-
     
-cout<<std::hex<<;
+cout<<std::hex<<endl;
 
 for (auto element :mojv )
     cout<<(int)element<<endl;;
-
 }
-
 
 int main ()
 {
-
-
 copiuj();
-
 return 0;
 }
 
