cd4b45f42181b887b3fdff85b87658b74891fe24
Marcel <ahimm44@gmail.com>
2019-01-09 09:31:23 +0100

    zutproj001

---
+++ net/ethFinal2/build-ethfinal2-Desktop_Qt_5_11_2_GCC_64bit-Default/CMakeFiles/pr.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcin/Desktop/kod/net/ethFinal2/ethfinal2/Ethernetdriverserver.cpp" "/home/marcin/Desktop/kod/net/ethFinal2/build-ethfinal2-Desktop_Qt_5_11_2_GCC_64bit-Default/CMakeFiles/pr.dir/Ethernetdriverserver.cpp.o"
+  "/home/marcin/Desktop/kod/net/ethFinal2/ethfinal2/MSGveryficator.cpp" "/home/marcin/Desktop/kod/net/ethFinal2/build-ethfinal2-Desktop_Qt_5_11_2_GCC_64bit-Default/CMakeFiles/pr.dir/MSGveryficator.cpp.o"
+  "/home/marcin/Desktop/kod/net/ethFinal2/ethfinal2/main.cpp" "/home/marcin/Desktop/kod/net/ethFinal2/build-ethfinal2-Desktop_Qt_5_11_2_GCC_64bit-Default/CMakeFiles/pr.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
