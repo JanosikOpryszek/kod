0753475aba06e71308aec988163a72832397cb48
JanosikOpryszek <ahimm44@gmail.com>
2019-02-03 11:56:03 -0800

    gtw

--- ZUT/projektsend/proj001/main.cpp
+++ ZUT/projektsend/proj001/main.cpp
@@ -173,8 +173,8 @@ GtkApplication *app;
 
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  //status = g_application_run (G_APPLICATION (app), argc, argv);
-  status = g_application_run (G_APPLICATION (app));
+  g_application_run (G_APPLICATION (app), argc, argv);
+
   g_object_unref (app);
 
   //return status;
