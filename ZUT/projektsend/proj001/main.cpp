eded88844e1e797c29bf061d11eae6eb8417579e
JanosikOpryszek <ahimm44@gmail.com>
2019-02-17 05:40:27 -0800

    kolejne2

--- ZUT/projektsend/proj001/main.cpp
+++ ZUT/projektsend/proj001/main.cpp
@@ -139,20 +139,9 @@ else
 }
 
 
-//reading and showing messages part
 
 
 
-GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  g_application_run (G_APPLICATION (app), argc, argv);
-
-  g_object_unref (app);
-
-  //return status;
 
 
 return 0;
